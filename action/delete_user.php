4d92e93f75f18d57032b7808bd5162a631259275
thanhlam1909 <lethanhlam9287@gmail.com>
2023-08-20 16:15:46 +0700

    cap nhat file

--- action/delete_user.php
+++ action/delete_user.php
@@ -4,21 +4,25 @@ include_once 'connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
     $user_id = $_GET["id"];
 
-    // Xóa người dùng từ các bảng liên quan dựa trên ID
-    $sql_delete_user = "DELETE FROM users WHERE user_id = $user_id";
+    // Xóa các đơn hàng liên quan đến người dùng
     $sql_delete_orders = "DELETE FROM orders WHERE user_id = $user_id";
-    // Thêm các truy vấn xóa từ các bảng khác ở đây...
 
-    if ($conn->query($sql_delete_user) === TRUE && $conn->query($sql_delete_orders) === TRUE) {
-        // Thêm kiểm tra cho các truy vấn xóa từ các bảng khác ở đây...
+    if ($conn->query($sql_delete_orders) === TRUE) {
+        // Sau khi xóa các đơn hàng liên quan, tiến hành xóa người dùng
+        $sql_delete_user = "DELETE FROM users WHERE user_id = $user_id";
 
-        echo "<script>alert('Đã xóa người dùng và dữ liệu liên quan thành công');</script>";
-        echo "<script>window.location = '/layout/display_users_controller.php';</script>";
+        if ($conn->query($sql_delete_user) === TRUE) {
+            echo "<script>alert('Đã xóa người dùng và dữ liệu liên quan thành công');</script>";
+        } else {
+            echo "<script>alert('Đã xóa người dùng thành công, nhưng xảy ra lỗi khi xóa dữ liệu liên quan');</script>";
+            echo "Lỗi: " . $conn->error;
+        }
     } else {
-        echo "<script>alert('Đã xóa người dùng và dữ liệu liên quan thất bại');</script>";
-        echo "<script>window.location = '/layout/display_users_controller.php';</script>";
+        echo "<script>alert('Xóa các đơn hàng liên quan thất bại');</script>";
         echo "Lỗi: " . $conn->error;
     }
+
+    echo "<script>window.location = '/layout/display_users_controller.php';</script>";
 }
 
 $conn->close();
